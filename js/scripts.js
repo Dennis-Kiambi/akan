6579ea7fa7c7e44e34dbb452f3b2951756b6deb2
Dennis-Kiambi <denniskiriinya@gmail.com>
2020-08-24 15:13:14 +0300

    add the calculateName function

---
+++ js/scripts.js
@@ -0,0 +1,32 @@
+
+const form = document.getElementById('form')
+
+function calculateName() {
+
+    const errorElement = document.getElementById('error');
+    const divResult = document.getElementById('divResult');
+    const txtDay = document.getElementById('dd');
+    const txtMonth = document.getElementById('mm');
+    const txtYear = document.getElementById('yyyy');
+    const selectGender = document.getElementById('gender');
+
+
+}
+/*
+
+form.addEventListener('submit', (e) => {
+    let messages = []
+    if (dd.value > 31 || dd.value <= 0) {
+        messages.push('Invalid day format')
+    }
+    if (mm.value > 12 || mm.value <= 0) {
+        messages.push('Invalid month format')
+    }
+    if (yyyy.value > 2020) {
+        messages.push('Year cannot be later than 2020')
+    }
+    if (messages.length > 0) {
+        e.preventDefault()
+        errorElement.innerText = messages.join(', ')
+    }
+})*/
