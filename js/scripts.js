3cf6eac90865bb4bc491e2e2de62fe9fcaf12254
Dennis-Kiambi <denniskiriinya@gmail.com>
2020-08-24 15:16:28 +0300

    add if statements to js file

--- js/scripts.js
+++ js/scripts.js
@@ -19,6 +19,32 @@ function calculateName() {
     errorElement.innerHTML = '';
     divResult.innerHTML = '';
 
+    if (day <= 0 || day > 31) {
+        errorElement.innerHTML = 'Day Must be between 1 and 30';
+        return false;
+    }
+
+    if (month <= 0 || month > 12) {
+        errorElement.innerHTML = 'Month Must be between 1 and 12';
+        return false;
+    }
+
+    if (year.length !== 4) {
+        errorElement.innerHTML = 'Year must be 4 digits';
+        return false;
+    }
+
+    if (year <= 0 || year > 2020) {
+        errorElement.innerHTML = 'Year cannot be later than 2020';
+        return false;
+    }
+
+    if (gender == 0) {
+        errorElement.innerHTML = 'Please Select Gender';
+        return false;
+    }
+
+    errorElement.innerHTML = 'Eevrything Is Ok';
 
 
 
